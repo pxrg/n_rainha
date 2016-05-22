38a82f200d37ddff64288227fd66e385371fa72a
pxrg <prxgomes@gmail.com>
2016-05-22 01:27:01 -0300

    Inicio de AG e conclusao do tempera_simulada

--- source/n_rainha.py
+++ source/n_rainha.py
@@ -3,6 +3,7 @@
 import os
 import random
 import json
+import time
 
 
 class NRainha(object):
@@ -21,8 +22,8 @@ class NRainha(object):
         self.populacao = []
         self.qtd_iteracoes = 100
         # Parametros do algoritmo genetico
-        self.mutacao = 0.05
-        self.cruzamento = 0.9
+        self.taxa_mutacao = 0.05
+        self.taxa_cruzamento = 0.9
         self._cache = {}
         if not self.carregar_dados():
             for x in xrange(qtd_amostras):
@@ -69,43 +70,61 @@ class NRainha(object):
         self._cache[(linha, col,)] = diag
         return diag[::]
 
-    def _funcao_objetivo_unitaria(self, pop_aux, linha, col):
-        conflitos = pop_aux.count(col) -1 # Verifica conflitos na horizontal
-        if conflitos <= 0:
-            conflitos = 0
+    def _funcao_objetivo_unitaria(self, tab_aux, linha, col):
+        """Funcao que calcula a funcao objetivo para uma unica peca"""
+        # Verifica conflitos na horizontal
+        conflitos = tab_aux.count(col) -1
+        # if conflitos <= 0:
+        #     conflitos = 0
+        # Gera uma lista com as diagonais para a posicao
         diagonais = self.calc_diag(linha, col)
+        tam_tab = len(tab_aux) - 1
+        # Verifica para as diagonais se existe algum conflito
         for diag in diagonais:
-            if pop_aux[diag[0]] == diag[1]:
+            if tab_aux[diag[0]] == diag[1]:
                 conflitos += 1
         return conflitos
 
     def _funcao_objetivo_tabuleiro(self, tabuleiro):
+        """Funcao que calcula a funcao objetivo para todo o tabuleiro"""
         conflitos = 0
         for idx, indiv in enumerate(tabuleiro):
             conflitos += self._funcao_objetivo_unitaria(tabuleiro, idx, indiv)
         return conflitos
 
     def _criar_objeto_resultado(self, tabuleiro, indice_populacao):
+        """Objeto de saida apos processamento """
         return { 'fo' : self.n_rainha * 999, 'tabuleiro': tabuleiro,
-                       'indice': indice_populacao, 'iteracao': 0 }
+                       'indice': indice_populacao, 'iteracao': 0,
+                       'inicio':  time.time(), 'termino': time.time()}
+
+    def _selecionar_menor_fo_para_coluna(self, tabuleiro, x, saida):
+        """Realiza o calculo de conflitos para cada linha na coluna informada
+             e seleciona o menor valor encontrado
+        """
+        _fos_ = []
+        for y in xrange(self.n_rainha):
+            fo = self._funcao_objetivo_unitaria(tabuleiro, x, y)
+            _fos_.append(fo)
+            saida['iteracao'] += 1
+        min_val = min(_fos_)
+        tabuleiro[x] = _fos_.index(min_val)
+
+    def _validar_saida_tabuleiro(self, tabuleiro, saida, indice_populacao):
+        fo = self._funcao_objetivo_tabuleiro(tabuleiro)
+        if saida['fo'] >= fo:
+            saida['fo'] = fo
+            saida['tabuleiro'] = tabuleiro
+            saida['indice'] = indice_populacao
 
     def _executar_subida_encosta(self, tabuleiro, indice_populacao):
         pos = 1
         saida = self._criar_objeto_resultado(tabuleiro, indice_populacao)
         for count in xrange(self.qtd_iteracoes):
             for x in xrange(pos, self.n_rainha):
-                _fos_ = []
-                for y in xrange(self.n_rainha):
-                    _fos_.append(self._funcao_objetivo_unitaria(tabuleiro, x, y))
-                    saida['iteracao'] += 1
-                min_val = min(_fos_)
-                tabuleiro[x] = _fos_.index(min_val)
-                fo = self._funcao_objetivo_tabuleiro(tabuleiro)
-                if saida['fo'] >= fo:
-                    saida['fo'] = fo
-                    saida['tabuleiro'] = tabuleiro
-                    saida['indice'] = indice_populacao
-                if fo == 0:
+                self._selecionar_menor_fo_para_coluna(tabuleiro, x, saida)
+                self._validar_saida_tabuleiro(tabuleiro, saida, indice_populacao)
+                if saida['fo'] == 0:
                     return saida
         return saida
 
@@ -118,42 +137,129 @@ class NRainha(object):
             # Valor da tempera para a iteracao
             tempera = (tempera_base - count)
             for x in xrange(pos, self.n_rainha):
-                _fos_ = []
                 if tempera > random.randint(0, 100):
                     tabuleiro[x] = random.randint(0, self.n_rainha -1)
                 else:
-                    for y in xrange(self.n_rainha):
-                        _fos_.append(self._funcao_objetivo_unitaria(tabuleiro, x, y))
-                        saida['iteracao'] += 1
-                    min_val = min(_fos_)
-                    tabuleiro[x] = _fos_.index(min_val)
-                fo = self._funcao_objetivo_tabuleiro(tabuleiro)
-                if saida['fo'] >= fo:
-                    saida['fo'] = fo
-                    saida['tabuleiro'] = tabuleiro
-                    saida['indice'] = indice_populacao
-                    if fo == 0:
-                        return saida
+                    self._selecionar_menor_fo_para_coluna(tabuleiro, x, saida)
+                self._validar_saida_tabuleiro(tabuleiro, saida, indice_populacao)
+                if saida['fo'] == 0:
+                    return saida
         return saida
 
-
     def subida_encosta(self):
-        # tabuleiro = self.populacao[0][::]
+        """ Metodo que para cada item da populacao e realizada
+              a subida encosta """
         self.resultado_sb = []
         for indice, item in enumerate(self.populacao):
-            self.resultado_sb.append(self._executar_subida_encosta(item[::], indice))
+            saida = self._executar_subida_encosta(item[::], indice)
+            saida['termino'] = time.time()
+            self.resultado_sb.append(saida)
         return self.resultado_sb
 
     def tempera_simulada(self):
-        # tabuleiro = self.populacao[0][::]
+        """ Metodo que para cada item da populacao e realizada
+              a tempera simulada"""
         self.resultado_sb = []
         for indice, item in enumerate(self.populacao):
-            self.resultado_sb.append(self._executar_tempera_simulada(item[::], indice))
+            saida = self._executar_tempera_simulada(item[::], indice)
+            saida['termino'] = time.time()
+            self.resultado_sb.append(saida)
         return self.resultado_sb
 
-    def buscar_alg_genetico(self, geracao):
+    def _seleciona_individuos(self, populacao):
+        # return self._seleciona_individuos_roleta(populacao[::])
+        return self._seleciona_individuos_torneio(populacao[::])
+
+    def _seleciona_individuos_torneio(self, populacao):
+        selecao = []
+        while len(selecao) < 2 and len(populacao) > 0:
+            tam_pop = len(populacao) - 1
+            a = random.randint(0, tam_pop)
+            b = random.randint(0, tam_pop)
+            while a == b:
+                b = random.randint(0, tam_pop)
+            indiv_a = populacao[a]
+            indiv_b = populacao[b]
+            fo_a = self._funcao_objetivo_tabuleiro(indiv_a)
+            fo_b = self._funcao_objetivo_tabuleiro(indiv_b)
+            if fo_a < fo_b:
+                selecao.append(indiv_a)
+                populacao.remove(indiv_a)
+            else:
+                selecao.append(indiv_b)
+                populacao.remove(indiv_b)
+        return selecao
+
+    def _seleciona_individuos_roleta(self, populacao):
+        _fos_ = []
+        total_fo = 0.0
+        for idx, item in enumerate(populacao):
+            fo = self._funcao_objetivo_tabuleiro(item)
+            if fo == 0:
+                print idx, item
+            total_fo += fo
+            _fos_.append((fo, idx))
+        _fos_.sort()
+        selecao = []
+        while len(selecao) < 2:
+            porcentagem = random.random() * total_fo
+            acumulado = 0
+            for item in _fos_:
+                fo_item = item[0]
+                if fo_item == 0:
+                    fo_item = 1
+                acumulado += total_fo / fo_item
+                if (porcentagem < acumulado) and selecao.count(self.populacao[item[1]]) == 0:
+                    selecao.append(self.populacao[item[1]])
+                    break
+
+        del _fos_, total_fo
+        return selecao
+
+    def _realiza_cruzamento(self, pai_1, pai_2):
+        individuo_1  = pai_1[::]
+        individuo_2  = pai_2[::]
+        posicao_corte = (len(individuo_1) / 2)
+        if random.random() < self.taxa_cruzamento:
+            if random.random() < 0.5:
+                posicao_corte -= 1
+            aux = individuo_1[posicao_corte:]
+            individuo_1[posicao_corte:] = individuo_2[posicao_corte:]
+            individuo_2[posicao_corte:] = aux
+        return [individuo_1, individuo_2]
+
+    def _realiza_mutacao(self, individuo):
+        if random.random() < self.taxa_mutacao:
+            individuo[random.randint(0, self.n_rainha - 1)] = random.randint(0, self.n_rainha - 1)
+        return individuo
+
+    def _avaliar_populacao(self, saida, populacao, geracao):
+        for idx, item_pop in enumerate(populacao):
+            obj_saida = self._criar_objeto_resultado(item_pop, idx)
+            obj_saida['iteracao'] = geracao
+            obj_saida['fo'] = self._funcao_objetivo_tabuleiro(item_pop)
+            if obj_saida['fo'] < saida[idx]['fo'] :
+                saida[idx] = obj_saida
+
+    def alg_genetico(self, geracao):
+        saida = [self._criar_objeto_resultado(None, 0) for e in xrange(len(self.populacao))]
         for ger in xrange(geracao):
-            pass
+            nova_populacao = []
+            while len(nova_populacao) < len(self.populacao):
+                # executa selecao
+                pai_1, pai_2 = self._seleciona_individuos(self.populacao)
+                # realiza o cruzamento
+                filhos = self._realiza_cruzamento(pai_1, pai_2)
+                # verifica mutacao
+                self._realiza_mutacao(filhos[0])
+                self._realiza_mutacao(filhos[1])
+                # Adiciona a nova populacao
+                nova_populacao.extend(filhos)
+            # substitui populacao
+            self.populacao = None
+            self.populacao = nova_populacao
+            self._avaliar_populacao(saida, self.populacao, ger)
+        return saida
 
     def imprime_tabuleiro(self, tabuleiro):
         size = len(tabuleiro)
--- source/n_rainha_test.py
+++ source/n_rainha_test.py
@@ -4,8 +4,14 @@ from n_rainha import *
 
 import unittest
 
+def armazenar_dados(metodo, result):
+    # nome_arquivo = "metodo_%s.json"%(metodo)
+    # with (open(nome_arquivo, 'w')) as _file_:
+    #     _file_.write(json.dumps(result))
+    pass
+
 class NRainhaTest(unittest.TestCase):
-    qtd_n_rainha = 6
+    qtd_n_rainha = 8
     qtd_amostras = 10
     DEBUG = True
     # DEBUG = False
@@ -24,15 +30,25 @@ class NRainhaTest(unittest.TestCase):
         self.assertEquals(self.qtd_amostras, len(n_rainha.tabuleiros_iniciais))
         self.assertEquals(self.qtd_n_rainha, len(n_rainha.tabuleiros_iniciais[0]))
 
-    def test_ArmazenarTabuleiroInicialEmArquivo(self):
+    def test_ArmazenarTabuleiroInicialEmArquivoERecuperar(self):
         n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
         n_rainha_2 = NRainha(self.qtd_n_rainha, self.qtd_amostras)
         self.assertEquals(n_rainha.tabuleiros_iniciais, n_rainha_2.tabuleiros_iniciais)
 
-    def _test_ExecutarSubidaDeEncostaEModificarTabuleiro(self):
+    def test_ValidarFuncaoObjetivo(self):
+        n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
+        tabuleiro = range(self.qtd_n_rainha -1, -1, -1)
+        # Validacao do calculo para uma unica posicao
+        self.assertEquals(self.qtd_n_rainha -1, n_rainha._funcao_objetivo_unitaria(tabuleiro, tabuleiro[0], 0))
+        # Validacao do calculo para o tabuleiro
+        self.assertEquals((self.qtd_n_rainha -1) * self.qtd_n_rainha, n_rainha._funcao_objetivo_tabuleiro(tabuleiro))
+
+
+    def test_ExecutarSubidaDeEncostaEModificarTabuleiro(self):
         n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
         result = n_rainha.subida_encosta()
         self.assertEquals(self.qtd_amostras, len(result))
+        armazenar_dados("subida_encosta", result)
         self.print_log("")
         for item in result:
             self.print_log(item)
@@ -43,11 +59,37 @@ class NRainhaTest(unittest.TestCase):
         n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
         result = n_rainha.tempera_simulada()
         self.assertEquals(self.qtd_amostras, len(result))
+        armazenar_dados("tempera_simulada", result)
         self.print_log("")
-        # self.print_log(result)
+        # n_rainha.imprime_tabuleiro(result[0]['tabuleiro'])
         for item in result:
             self.print_log(item)
             self.assertNotEquals(n_rainha.populacao[item['indice']], item['tabuleiro'])
+
+    def test_AG_SelecionarIndividuos(self):
+        n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
+        selecionados = n_rainha._seleciona_individuos(n_rainha.populacao)
+        self.assertTrue(len(selecionados) == 2 )
+
+    def test_AG_SelecionarIndividuosRealizarCruzamento(self):
+        n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
+        selecionados = n_rainha._seleciona_individuos(n_rainha.populacao)
+        self.assertEquals( 2, len(selecionados) )
+        saida = n_rainha._realiza_cruzamento(selecionados[0], selecionados[1])
+        self.assertNotEquals(selecionados, saida)
+
+    def test_AG_ExecutarAlgoritomoGenetico(self):
+        n_rainha = NRainha(self.qtd_n_rainha, self.qtd_amostras)
+        result = n_rainha.alg_genetico(1000)
+        self.assertEquals(self.qtd_amostras, len(result))
+        armazenar_dados("alg_genetico", result)
+        self.print_log("")
+        # n_rainha.imprime_tabuleiro(result[0]['tabuleiro'])
+        for item in result:
+            self.print_log(item)
+            self.assertNotEquals(n_rainha.tabuleiros_iniciais[item['indice']], item['tabuleiro'])
+
+
 
 
 
